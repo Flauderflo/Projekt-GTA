abf9a4253c66b88c1387b9a699f9c56529335060
Enrico Janssen <enrico.janssen@ik.me>
2025-11-30 23:33:08 +0100

    chly python chegget und db versueche z lade

--- backend/app.py
+++ backend/app.py
@@ -9,8 +9,8 @@ app = Flask(__name__)
 CORS(app, origins=["*", "null"])  # allowing any origin as well as localhost (null)
 
 # SOLUTION TASK 4
-@app.route("/compute_mean", methods=["GET"])
-def compute_mean():
+@app.route("/compute", methods=["GET"])
+def compute():
     # retrieve column name from the request arguments
     col_name = str(request.args.get("column_name", "value"))
 
@@ -53,6 +53,12 @@ def get_mean_value_from_table(col_name):
     print(dblogin)
 
     # Read column from database
-    table_one_column = pd.read_sql(f"SELECT {col_name} FROM flask_exercise.test_data", engine)
+    table_one_column = pd.read_sql(f"SELECT {col_name} FROM gta25_g3.trajektorien", engine)
+    table_schulen = pd.read_sql(f"SELECT {col_name} FROM gta25_g3.schule", engine)
+    table_routen = pd.read_sql(f"SELECT {col_name} FROM gta25_g3.velovorzugslinien", engine)
     # compute mean
-    return table_one_column[col_name].mean()
+    table_one_column["route"] = table_one_column[col_name].route_finden()
+    table_one_column["end_schule"] = table_one_column[col_name].schule_finden
+
+    # write new data in db
+    return table_one_column
